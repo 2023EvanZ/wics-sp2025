fb651ee158e3d07748d6ed3ba133d96cd7b5c8d3
allenh99 <ahthing99@gmail.com>
2025-02-22 12:11:15 -0500

    initial frontend app working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoDisplay from "./components/Display"; // Adjust path based on new structure
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React-Django App</h1>
+      <VideoDisplay />
+    </div>
+  );
+};
+
+export default App;
