39cb1b65ffcdc5d2c9e175febf34d5e5df55c4c1
aleXyaO7 <alexyao0741@gmail.com>
2025-02-22 21:15:08 -0500

    added admin

--- city_app/admin.py
+++ city_app/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Business
+from .models import Business, Video
 
 class BusinessAdmin(admin.ModelAdmin):
-    fields = ["id", "name", "longitude", "latitude", "video", "description", "location", "hours", "contacts", "likes", "dislikes"]
+    fields = "__all__"
+
+class VideoAdmin(admin.ModelAdmin):
+    fields = "__all__"
 
 admin.site.register(Business, BusinessAdmin)
+admin.site.register(Video, VideoAdmin)
