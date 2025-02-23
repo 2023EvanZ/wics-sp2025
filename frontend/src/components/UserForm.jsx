a77df40b97ad25ff33c2af4268055f9d64b80d8b
allenh99 <ahthing99@gmail.com>
2025-02-23 01:09:41 -0500

    created userform

---
+++ frontend/src/components/UserForm.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const UserForm = () => {
+    <form onSubmit={handleSubmit}>
+        <input type="text" name="location" placeholder="Location" value={formData.location} onChange={handleChange} required />
+    </form>
+}
+export default UserForm
